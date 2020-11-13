463ab5a9aca087fd926cbaff0bb49e997de0ed08
jk086 <jiakang0216@gmail.com>
2020-11-13 15:03:39 +0800

    update

--- cart/routes/web.php
+++ cart/routes/web.php
@@ -17,6 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/insertCategory', function () {
+    return view('insertCategory');
+});
+
+Route::get('/contactus', function () {
+    return view('contact');
+});
+
+
+
+Route::post('/insertCategory/store', [App\Http\Controllers\CategoryController::class, 'store'])->name('addCategory');
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
